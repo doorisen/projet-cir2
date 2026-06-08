7eac23e45eb3f20298ecbc196f2973061c438ff5
Valentin TANG-PATUREL <valentin.tang-paturel@isen-ouest.yncrea.fr>
2026-06-08 16:56:18 +0200

    lien css

--- details.php
+++ details.php
@@ -16,7 +16,7 @@ $isCreation = ($isAdmin && empty($id));
     <title><?= $isCreation ? 'Nouveau point' : 'Détails' ?> - IRVE Bretagne <?= $isAdmin ? '- Administration' : '' ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="/assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/main.css">
 </head>
 <body class="d-flex flex-column min-vh-100 custom-bg <?= $isAdmin ? 'admin-mode' : '' ?>">
 
